6484db674d76878100e65accd18b597d8c6f779f
katyjane8 <katy.jane8@gmail.com>
2018-02-07 10:43:02 -0700

    PORO attributes

--- app/models/fuel.rb
+++ app/models/fuel.rb
@@ -1,3 +1,15 @@
-class Fuel
+class FuelStations
+  attr_reader :name,
+              :address,
+              :fuel_type,
+              :distance,
+              :access_times
 
-end 
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @address = attributes[:address]
+    @fuel_type = attributes[:fuel_type]
+    @distance = attributes[:distance]
+    @access_times = attributes[:access_times]
+  end
+end
